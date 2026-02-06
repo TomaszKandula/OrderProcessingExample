9304977053ff74e0322058666671dcc6093b3560
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-06 01:25:01 +0100

    feat: add program w/application

---
+++ OrderProcessing.Application/Application.cs
@@ -0,0 +1,37 @@
+
+using OrderProcessing.Domain;
+using OrderProcessing.Infrastructure.Services.Abstractions;
+
+namespace OrderProcessing.Application;
+
+public class Application(IOrderService orderService, ILoggerService loggerService)
+{
+    public void Run()
+    {
+        loggerService.LogInfo("Order Processing System started...");
+
+        // This simulates Order Processing System
+        var exampleOrder1 = new OrderDto
+        {
+            Id = 1,
+            Description = "Keyboard"
+        };
+
+        var exampleOrder2 = new OrderDto
+        {
+            Id = 4,
+            Description = "Display"
+        };
+
+        var tasks = new Task[6];
+        tasks[0] = Task.Run(() => { orderService.ProcessOrder(1); });
+        tasks[1] = Task.Run(() => { orderService.ProcessOrder(2); });
+        tasks[2] = Task.Run(() => { orderService.CreateOrderEntry(exampleOrder1); }); // This will throw 'ArgumentException'
+        tasks[3] = Task.Run(() => { orderService.CreateOrderEntry(exampleOrder2); });
+        tasks[4] = Task.Run(() => { orderService.ProcessOrder(3); }); // This will throw 'KeyNotFoundException'
+        tasks[5] = Task.Run(() => { orderService.ProcessOrder(-1); }); // This will throw 'ArgumentException'
+
+        Task.WaitAll(tasks);
+        loggerService.LogInfo("Job execution completed.");
+    }
+}
---
+++ OrderProcessing.Application/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrderProcessing.Infrastructure.Repositories;
+using OrderProcessing.Infrastructure.Services;
+using OrderProcessing.Infrastructure.Services.Abstractions;
+
+namespace OrderProcessing.Application;
+
+internal abstract class Program
+{
+    /// <summary>
+    /// Application main entry point.
+    /// </summary>
+    private static void Main()
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+
+        var serviceProvider = services.BuildServiceProvider();
+        var application = serviceProvider.GetService<Application>();
+        if (application is null)
+            throw new InvalidOperationException("Oh no! Application service not found!");
+
+        application.Run();
+    }
+
+    /// <summary>
+    /// Service registration.
+    /// </summary>
+    /// <param name="services">Service collection.</param>
+    private static void ConfigureServices(ServiceCollection services)
+    {
+        services
+            .AddTransient<Application>()
+            .AddScoped<IOrderValidator, OrderValidator>()
+            .AddScoped<IOrderService, OrderService>()
+            .AddSingleton<ILoggerService, LoggerService>()
+            .AddSingleton<IOrderRepository, OrderRepository>();
+    }
+}
